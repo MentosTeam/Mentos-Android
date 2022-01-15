42cdf4d2a6f38099691b37a45361550cf1877407
gaeun23 <0423g@naver.com>
2022-01-15 17:15:01 +0900

    #1 Package 설정

---
+++ app/src/main/java/com/mentos/mentosandroid/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mentos.mentosandroid.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mentos.mentosandroid.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
