660bd556dfa1882ce2f7334f57b9437f179ed1e0
gaeun23 <0423g@naver.com>
2022-01-19 02:18:32 +0900

    #5 [layout] Bottom navi 레이아웃

--- app/src/main/java/com/mentos/mentosandroid/ui/main/MainActivity.kt
+++ app/src/main/java/com/mentos/mentosandroid/ui/main/MainActivity.kt
@@ -3,10 +3,14 @@ package com.mentos.mentosandroid.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mentos.mentosandroid.R
+import com.mentos.mentosandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
