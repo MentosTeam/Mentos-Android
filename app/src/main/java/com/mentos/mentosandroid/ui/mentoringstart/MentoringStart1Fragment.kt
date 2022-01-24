7bfb99d2c3e01a90c4bdbd77ef4d0856a4a7469d
junwon1997 <junwon20170339@gmail.com>
2022-01-24 14:26:46 +0900

    #10 [layout] 멘토링 시작하기 레이아웃

---
+++ app/src/main/java/com/mentos/mentosandroid/ui/mentoringstart/MentoringStart1Fragment.kt
@@ -0,0 +1,37 @@
+package com.mentos.mentosandroid.ui.mentoringstart
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mentos.mentosandroid.R
+import com.mentos.mentosandroid.databinding.FragmentMentoringStart1Binding
+import com.mentos.mentosandroid.util.navigate
+import com.mentos.mentosandroid.util.popBackStack
+
+class MentoringStart1Fragment : Fragment() {
+    private lateinit var binding: FragmentMentoringStart1Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMentoringStart1Binding.inflate(inflater,container,false)
+
+        binding.mentoringStart1BackArrowIv.setOnClickListener {
+            popBackStack()
+        }
+
+        binding.mentoringStart1ButtonBtn.setOnClickListener {
+            navigate(R.id.action_mentoringStart1Fragment_to_mentoringStart2Fragment22)
+        }
+
+
+        return binding.root
+
+    }
+
+
+}
---
+++ app/src/main/java/com/mentos/mentosandroid/ui/mentoringstart/MentoringStart2Fragment.kt
@@ -0,0 +1,35 @@
+package com.mentos.mentosandroid.ui.mentoringstart
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mentos.mentosandroid.R
+import com.mentos.mentosandroid.databinding.FragmentMentoringStart2Binding
+import com.mentos.mentosandroid.util.navigate
+import com.mentos.mentosandroid.util.popBackStack
+
+class MentoringStart2Fragment : Fragment() {
+    private lateinit var binding : FragmentMentoringStart2Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMentoringStart2Binding.inflate(inflater,container,false)
+
+        binding.mentoringStart2BackArrowIv.setOnClickListener {
+            popBackStack()
+        }
+
+        binding.mentoringStart2ButtonBtn.setOnClickListener {
+            navigate(R.id.action_mentoringStart2Fragment2_to_mentoringStart3Fragment2)
+        }
+
+
+
+        return binding.root
+    }
+}
