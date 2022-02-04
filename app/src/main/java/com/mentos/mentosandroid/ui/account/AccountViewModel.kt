58e32d702a8b6aace9454ab4d75540aa72398d84
gaeun23 <0423g@naver.com>
2022-02-04 17:36:46 +0900

    #84 [feat] AccountViewModel

--- app/src/main/java/com/mentos/mentosandroid/ui/account/AccountViewModel.kt
+++ app/src/main/java/com/mentos/mentosandroid/ui/account/AccountViewModel.kt
@@ -1,5 +1,6 @@
 package com.mentos.mentosandroid.ui.account
 
+import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,10 +9,46 @@ import com.mentos.mentosandroid.util.MediatorLiveDataUtil
 class AccountViewModel : ViewModel() {
     val introduce = MutableLiveData("")
 
+    private val _image = MutableLiveData<Uri?>()
+    val image: MutableLiveData<Uri?> = _image
+
+    private val _selectedCategory = MutableLiveData<List<Int>>()
+    val selectedCategory: LiveData<List<Int>> = _selectedCategory
+
+    private val tempCategory = mutableListOf<Int>()
+
+    private val _selectedState = MutableLiveData<Int>()
+    val selectedState: LiveData<Int> = _selectedState
+
     private val _canRegister = MediatorLiveDataUtil.initMediatorLiveData(
-        listOf(
-            introduce
-        )
-    ) { requireNotNull(introduce.value).isNotBlank() }
+        listOf(introduce)
+    ) { canRegisterCheck() }
     val canRegister: LiveData<Boolean> = _canRegister
+
+    private fun canRegisterCheck() =
+        requireNotNull(introduce.value).isNotBlank()
+                && introduce.value!!.length > 9
+
+    fun setCategory(category: Int) {
+        tempCategory.add(category)
+        _selectedCategory.value = tempCategory.toMutableList()
+    }
+
+    fun removeCategory(category: Int) {
+        tempCategory.remove(category)
+        _selectedCategory.value = tempCategory.toMutableList()
+    }
+
+    fun clearCategory() {
+        tempCategory.clear()
+        _selectedCategory.value = tempCategory.toMutableList()
+    }
+
+    fun setState(state: Int) {
+        _selectedState.value = state
+    }
+
+    fun setImage(imgUri: Uri) {
+        _image.value = imgUri
+    }
 }
