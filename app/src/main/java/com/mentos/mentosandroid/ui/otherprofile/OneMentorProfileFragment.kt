747be06e21a0a594627c739be4ae7763472c8aa5
HYUNJIN LEE <dlguswls456@naver.com>
2022-02-07 02:35:35 +0900

    #92 [feat, layout] 프로필 더보기 버튼 / 멘토스 수집 판 수정

--- app/src/main/java/com/mentos/mentosandroid/ui/otherprofile/OneMentorProfileFragment.kt
+++ app/src/main/java/com/mentos/mentosandroid/ui/otherprofile/OneMentorProfileFragment.kt
@@ -1,13 +1,16 @@
 package com.mentos.mentosandroid.ui.otherprofile
 
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import androidx.viewpager2.widget.MarginPageTransformer
 import com.mentos.mentosandroid.databinding.FragmentOneMentorProfileBinding
+import com.mentos.mentosandroid.ui.profile.ProfileMentosVPAdapter
 import com.mentos.mentosandroid.ui.profile.ProfileViewModel
 import com.mentos.mentosandroid.util.SharedPreferenceController
 import com.mentos.mentosandroid.util.navigateWithData
@@ -25,6 +28,7 @@ class OneMentorProfileFragment : Fragment() {
     ): View {
         binding = FragmentOneMentorProfileBinding.inflate(inflater, container, false)
         initViewModel()
+        initMentosVP()
         initSex()
         setBackBtnClickListener()
         setNavigateWithMentorId()
@@ -36,6 +40,31 @@ class OneMentorProfileFragment : Fragment() {
         binding.profileViewModel = profileViewModel
         binding.lifecycleOwner = this
     }
+
+    private fun initMentosVP() {
+        val mentosVPAdapter = ProfileMentosVPAdapter(this)
+        mentosVPAdapter.mentosList = profileViewModel.mentorMentosList.value!!
+
+        val mentosVP = binding.mentorProfileMentoringMentosVp
+
+        // 좌/우 노출되는 크기를 크게하려면 offsetPx 증가
+        val leftOffsetPx = 30.dpToPx(resources.displayMetrics)
+        val rightOffsetPx = 21.dpToPx(resources.displayMetrics)
+        mentosVP.setPadding(leftOffsetPx, 0, rightOffsetPx, 0)
+
+        // 페이지간 마진 크게하려면 pageMarginPx 증가
+        val pageMarginPx = 1.dpToPx(resources.displayMetrics)
+        val marginTransformer = MarginPageTransformer(pageMarginPx)
+        mentosVP.setPageTransformer(marginTransformer)
+
+        mentosVP.offscreenPageLimit = 3
+
+        mentosVP.adapter = mentosVPAdapter
+        mentosVP.currentItem = mentosVPAdapter.itemCount
+    }
+
+    fun Int.dpToPx(displayMetrics: DisplayMetrics): Int = (this * displayMetrics.density).toInt()
+
 
     private fun initSex() {
         val isOpen = SharedPreferenceController.getOpenSex(requireContext())
---
+++ app/src/main/java/com/mentos/mentosandroid/ui/profile/MentosBoardFragment.kt
@@ -0,0 +1,53 @@
+package com.mentos.mentosandroid.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import com.mentos.mentosandroid.databinding.FragmentMentosBoardBinding
+
+class MentosBoardFragment() : Fragment() {
+
+    companion object {
+        fun newInstance(mentosSubList: ArrayList<Int>): MentosBoardFragment {
+            val args = Bundle().apply {
+                putIntegerArrayList("mentosSubList", mentosSubList)
+            }
+
+            val fragment = MentosBoardFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    lateinit var binding: FragmentMentosBoardBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMentosBoardBinding.inflate(inflater, container, false)
+
+        var mentosSubList = requireArguments().getIntegerArrayList("mentosSubList")
+
+        val mentosCount = mentosSubList!!.size
+        if (mentosSubList.size < 10) {
+            for (i in mentosSubList.size until 10) {
+                mentosSubList.add(i, 0)
+            }
+        }
+
+        binding.mentorProfileMentoringMentosCountTv.text = "${mentosCount}/10"
+
+        //내부 리사이클러뷰 연동
+        val mentosAdapter = ProfileMentosRVAdapter()
+        mentosAdapter.mentosList = mentosSubList
+        mentosAdapter.notifyDataSetChanged()
+        binding.mentorProfileMentoringMentosRv.adapter = mentosAdapter
+
+        return binding.root
+    }
+}
